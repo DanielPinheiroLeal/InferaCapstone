defe26e720259925da33fcd037e1ff75c4674cf0
Saad Hussain <saad.h@outlook.com>
2021-03-23 00:32:47 -0400

    updated server based on database changes

--- webapp/server/README.md
+++ webapp/server/README.md
@@ -6,16 +6,19 @@ Backend server in Python using Flask.
 
 - Python 3.x
 - Flask - `pip install flask`
-- Neo4j (see ... (TODO))
+- Neo4j (see PPD under Creation --> Database)
 
 ## How to start server
 
 - Start neo4j database
 - `python api.py`
+    - Command line arguments:
+        - `-d`, `--debug`: Optional. Set to `True` to turn on debug mode. Default: `False` \
+        e.g. `python api.py -d True`
 
 ## Examples
 
 Try visiting the following URLs:
-- http://localhost:5000/search?author=Jane%20Doe%2010000
-- http://localhost:5000/search?title=Paper%2010000
+- http://localhost:5000/search?author=Jane%20Doe%2010000&mode=exact
+- http://localhost:5000/search?title=Paper%2010000&mode=related
 - http://localhost:5000/search?x=0.6&y=0.7&z=0.1
--- webapp/server/database.py
+++ webapp/server/database.py
@@ -5,27 +5,65 @@ import time
 import random
 from pathlib import Path
 from neo4j import GraphDatabase
-from flask import g
 
-def get_db(pdf_path="", num_neighbours=100):
-    if "db" not in g:
-        g.db = DbDriver("bolt://localhost:7687", "neo4j", "capstone", num_neighbours, pdf_path, True)
-    return g.db
+db_account = {
+    "user": "neo4j",
+    "password": "capstone"
+}
 
-def close_db(e=None):
-    db = g.pop("db", None)
-    if db is not None:
-        db.close()
+def get_db(pdf_path="", uri="bolt://localhost:7687", user=db_account["user"], 
+           password=db_account["password"], num_neighbours=25, debug_info=False):
+    '''
+    Connect to neo4j database
 
-def init_db(pdf_path, num_neighbours=100):
+    Parameters:
+        pdf_path: [string] Top-level path to recursively load PDF files from to populate database
+        uri: [string] Database URI
+        user: [string] Database account username
+        password: [string] Database account password
+        num_neighbours: [int] Number of nearest neighbours to compute between points in graph database
+        debug_info: [bool] Whether to turn on or off debug info for database
+    Returns:
+        db: DbDriver instance
+    '''
+    print("[INFO]: Connecting to database")
+    db = DbDriver(uri, user, password, num_neighbours, pdf_path, debug_info)
+    return db
+
+def close_db(db):
+    '''
+    Close connection to database
+
+    Parameters:
+        db: DbDriver instance
+    '''
+    print("[INFO]: Closing connection to database")
+    db.close()
+
+def init_db(pdf_path, uri="bolt://localhost:7687", user=db_account["user"], 
+            password=db_account["password"], num_neighbours=25, debug_info=False):
+    '''
+    Populate database with PDFs and build kNN graph
+
+    Parameters:
+        pdf_path: [string] Top-level path to recursively load PDF files from to populate database
+        uri: [string] Database URI
+        user: [string] Database account username
+        password: [string] Database account password
+        num_neighbours: [int] Number of nearest neighbours to compute between points in graph database
+        debug_info: [bool] Whether to turn on or off debug info for database
+    '''
+    print("[INFO]: Initializing database")
     db = get_db(pdf_path=pdf_path, num_neighbours=num_neighbours)
     db.build_db()
     db.build_knn_graph()
+
 
 class DbDriver:
 
     # Connect to the DB
     def __init__(self, uri, user, password, numK, pdf_path, debug_info):
+
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
         self.graph_name = 'neuripsGraph' # Name of the GDS graph
         self.numK = numK # Number of neighbours for the KNN algorithm
@@ -50,7 +88,7 @@ class DbDriver:
             title = "Paper " + str(i) # Placeholder
             coord = [random.random(), random.random(), random.random()] # Placeholder
             self.insert_node(pdf, author, title, coord)
-            i += 1
+            i+=1
 
         if self.debug_info:
             t_final = time.perf_counter()
@@ -58,7 +96,9 @@ class DbDriver:
         print("\nDatabase successfully built! Number of papers: {}\n".format(i))
 
         if self.debug_info:
-            print("(INFO): {:.3f} s elapsed".format(t_final - t_init))
+            t_tot = t_final-t_init
+            print("(INFO): {:.3f} s elapsed".format(t_tot))
+            return t_tot
 
     # Destroy the DB and GDS KNN graph (if any)
     def destroy_db(self):
@@ -77,7 +117,9 @@ class DbDriver:
         print("\nDatabase successfully removed!\n")
 
         if self.debug_info:
-            print("(INFO): {:.3f} s elapsed".format(t_final - t_init))
+            t_tot = t_final-t_init
+            print("(INFO): {:.3f} s elapsed".format(t_tot))
+            return t_tot
 
     # Insert a node (paper) in the graph database
     def insert_node(self, pdf, author, title, coord):
@@ -100,33 +142,41 @@ class DbDriver:
 
         if self.debug_info:
             t_final = time.perf_counter()
-            print("(INFO): {:.3f} s elapsed".format(t_final - t_init))
+            t_tot = t_final-t_init
+            print("(INFO): {:.3f} s elapsed".format(t_tot))
+            return t_tot
 
     # Query the DB by author
-    def query_by_author(self, author):
+    # mode: 'exact' - get exact matches; 'related' - get related results
+    def query_by_author(self, author, mode):
         if self.debug_info:
             t_init = time.perf_counter()
 
         with self.driver.session() as session:
-            res = session.read_transaction(self._query_by_author, author)
+            res = session.read_transaction(self._query_by_author, author, mode)
 
         if self.debug_info:
             t_final = time.perf_counter()
-            print("(INFO): {:.3f} s elapsed".format(t_final - t_init))
+            t_tot = t_final-t_init
+            print("\n(INFO): {:.3f} s elapsed".format(t_tot))
+            return (res, t_tot)
 
         return res
 
     # Query the DB by title
-    def query_by_title(self, title):
+    # mode: 'exact' - get exact matches; 'related' - get related results
+    def query_by_title(self, title, mode):
         if self.debug_info:
             t_init = time.perf_counter()
 
         with self.driver.session() as session:
-            res = session.read_transaction(self._query_by_title, title)
+            res = session.read_transaction(self._query_by_title, title, mode)
 
         if self.debug_info:
             t_final = time.perf_counter()
-            print("(INFO): {:.3f} s elapsed".format(t_final - t_init))
+            t_tot = t_final-t_init
+            print("(INFO): {:.3f} s elapsed".format(t_tot))
+            return (res, t_tot)
 
         return res
 
@@ -140,7 +190,9 @@ class DbDriver:
 
         if self.debug_info:
             t_final = time.perf_counter()
-            print("(INFO): {:.3f} s elapsed".format(t_final - t_init))
+            t_tot = t_final-t_init
+            print("(INFO): {:.3f} s elapsed".format(t_tot))
+            return (res, t_tot)
 
         return res
 
@@ -174,47 +226,49 @@ class DbDriver:
 
     # Query DB by author. Return matching papers and their nearest neighbours
     @staticmethod
-    def _query_by_author(tx, author):
-        result = tx.run("MATCH (p1:Paper)-[s:SIMILAR_TO]->(p2:Paper) WHERE p1.author \
-                        = $author RETURN collect(DISTINCT p1) AS P1, collect(DISTINCT p2) \
-                        AS P2 ", author=author)
+    def _query_by_author(tx, author, mode):
+        if mode == "exact": # Return only exact matches
+            result = tx.run("MATCH (p1:Paper)-[s:SIMILAR_TO]->(p2:Paper) WHERE p1.author \
+                        = $author RETURN collect(DISTINCT p1) AS P ", author=author)
+        elif mode == "related": # Return only related papers
+            result = tx.run("MATCH (p1:Paper)-[s:SIMILAR_TO]->(p2:Paper) WHERE p1.author \
+                        = $author RETURN collect(DISTINCT p2) AS P ", author=author)
+        else:
+            print("Query mode not supported!")
+            return
 
-        p1_list = []
-        p2_list = []
+        p_list = []
 
         for r in result:
-            for p1 in r["P1"]:
-                prop = p1._properties
-                p1_list.append({"id": p1._id, "pdf": prop["pdf"], "author": prop["author"], \
-                                "title": prop["title"], "coord": prop["coord"]})
-            for p2 in r["P2"]:
-                prop = p2._properties
-                p2_list.append({"id": p2._id, "pdf": prop["pdf"], "author": prop["author"], \
+            for p in r["P"]:
+                prop = p._properties
+                p_list.append({"id": p._id, "pdf": prop["pdf"], "author": prop["author"], \
                                 "title": prop["title"], "coord": prop["coord"]})
 
-        return(p1_list, p2_list)
+        return p_list
 
     # Query DB by paper title. Return matching papers and their nearest neighbours
     @staticmethod
-    def _query_by_title(tx, title):
-        result = tx.run("MATCH (p1:Paper)-[s:SIMILAR_TO]->(p2:Paper) WHERE p1.title \
-                        = $title RETURN collect(DISTINCT p1) AS P1, collect(DISTINCT p2) \
-                        AS P2 ", title=title)
+    def _query_by_title(tx, title, mode):
+        if mode == "exact": # Return only exact matches
+            result = tx.run("MATCH (p1:Paper)-[s:SIMILAR_TO]->(p2:Paper) WHERE p1.title \
+                        = $title RETURN collect(DISTINCT p1) AS P ", title=title)
+        elif mode == "related":
+            result = tx.run("MATCH (p1:Paper)-[s:SIMILAR_TO]->(p2:Paper) WHERE p1.title \
+                        = $title RETURN collect(DISTINCT p2) AS P ", title=title)
+        else:
+            print("Query mode not supported!")
+            return
 
-        p1_list = []
-        p2_list = []
+        p_list = []
 
         for r in result:
-            for p1 in r["P1"]:
-                prop = p1._properties
-                p1_list.append({"id": p1._id, "pdf": prop["pdf"], "author": prop["author"], \
-                                "title": prop["title"], "coord": prop["coord"]})
-            for p2 in r["P2"]:
-                prop = p2._properties
-                p2_list.append({"id": p2._id, "pdf": prop["pdf"], "author": prop["author"], \
+            for p in r["P"]:
+                prop = p._properties
+                p_list.append({"id": p._id, "pdf": prop["pdf"], "author": prop["author"], \
                                 "title": prop["title"], "coord": prop["coord"]})
 
-        return(p1_list, p2_list)
+        return p_list
 
     # Query DB by coordinates. Return the nearest neighbours to the given coordinate
     @staticmethod
@@ -238,8 +292,5 @@ class DbDriver:
         result = tx.run("MATCH(p:Paper) DETACH DELETE p")
 
 if __name__ == "__main__":
-    # db_driver = DbDriver("bolt://localhost:7687", "neo4j", "capstone", 100, r"H:\Saad\University\Course Material\Fourth Year\Semester 2\ESC472\Project\NeuripsSplit\NeurIPS", True)
-    # db_driver.build_db()
-    # db_driver.build_knn_graph()
-    # db_driver.close()
-    init_db(r"H:\Saad\University\Course Material\Fourth Year\Semester 2\ESC472\Project\NeuripsSplit\NeurIPS")
+    pdf_path = r"H:\Saad\University\Course Material\Fourth Year\Semester 2\ESC472\Project\NeuripsSplit\NeurIPS"
+    init_db(pdf_path)
