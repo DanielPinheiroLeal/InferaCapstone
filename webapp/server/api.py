efdf2bdf3989c23d3b0b966007aa1f97cc8b0035
Saad Hussain <saad.h@outlook.com>
2021-04-26 05:44:16 -0400

    update startup, bug fixes

--- webapp/server/api.py
+++ webapp/server/api.py
@@ -2,32 +2,33 @@
 App backend API
 '''
 import argparse
-from pathlib import Path
 import atexit
+from pathlib import Path
+import urllib.parse
 from flask import Flask, jsonify, request, redirect, make_response
 from flask_cors import CORS
 import numpy as np
-import database
 from sklearn.manifold import TSNE
-import urllib.parse
+import database
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--model-path", help="Path to directory containing saved topic modelling files", required=True)
-parser.add_argument("-pdf", "--pdf-path", help="Path to directory containing pdf files", required=True)
-parser.add_argument("-text", "--text-path", help="Path to directory containing text files", required=True)
-
-parser.add_argument("-d", "--debug", help="Whether to turn debug mode on or off. True/False", default=False, action = "store_true")
-parser.add_argument("-t", "--train", help="Train the model from scratch. True/False", default = False, action = "store_true")
+parser.add_argument("-r", "--db-uri", help="Database URI", default="bolt://localhost:7687")
+parser.add_argument("-u", "--user", help="Database account username", default="neo4j")
+parser.add_argument("-p", "--password", help="Database account password", default="capstone")
+parser.add_argument("-k", "--neighbours", help="Number of neighbours in knn graph", default=25, type=int)
+parser.add_argument("-lsi", "--numLSI", help="Number of LSI dimensions", default=10, type=int)
+parser.add_argument("-lda", "--numLDA", help="Number of LDA topics", default=10, type=int)
+parser.add_argument("-pdf", "--pdf-path", help="Path to directory containing pdf files", default="")
+parser.add_argument("-text", "--text-path", help="Path to directory containing text files", default="")
+parser.add_argument("-t", "--train", help="Train the model from scratch. True/False", default=False, action="store_true")
 parser.add_argument("-n", "--nodes", help="Rebuild database nodes. True/False", default=False, action="store_true")
-parser.add_argument("-k", "--neighbours", help="Number of neighbours in knn graph", required=False, default=25, type=int)
-parser.add_argument("-lsi", "--numLSI", help="Number of LSI dimensions", required=False, default=10, type=int)
-parser.add_argument("-lda", "--numLDA", help="Number of LDA topics", required=False, default=10, type=int)
 parser.add_argument("-c", "--convert", help="Convert PDFs to text files", default=False, action="store_true")
-
+parser.add_argument("-d", "--debug", help="Turn debug mode on or off. True/False", default=False, action="store_true")
 args = parser.parse_args()
+
 app = Flask(__name__)
 CORS(app)
-if args.debug:
-    app.config["DEBUG"] = True
 
 @app.route('/')
 def home():
@@ -97,10 +98,11 @@ def article_pdf_by_path(pdf_path):
     Example: http://localhost:5000/article/pdf_by_path/C%3A%5C%5Cpdf_files%5C%5Cfile1.pdf
     '''
     title = Path(pdf_path).stem
-    pdf_path=urllib.parse.unquote(pdf_path)
-    if(pdf_path[0]=="\\"):
-        pdf_path=pdf_path.replace("\\","/")
-    print(pdf_path)
+
+    pdf_path = urllib.parse.unquote(pdf_path)
+    if pdf_path[0]=="\\":
+        pdf_path = pdf_path.replace("\\","/")
+
     try:
         with open(pdf_path, "rb") as pdf:
             pdf_bytes = pdf.read()
@@ -112,58 +114,14 @@ def article_pdf_by_path(pdf_path):
     res.headers['Content-Disposition'] = 'inline; filename={}.pdf'.format(title)
     return res
 
-@app.route('/article/pdf_by_id/<paper_id>')
-def article_pdf_by_id(paper_id):
-    '''
-    Serve PDF files from the file system based on PDF id in database.
-
-    Parameters:
-        paper_id: PDF id in database
-
-    Example: http://localhost:5000/article/pdf_by_id/Phasor_Neural_Networks
-    '''
-    db_res = db.query_by_paper_id(int(paper_id), "exact")
+@app.route('/visualization/<paper_id>')
+def visualization(paper_id):
+    itself = db.query_by_paper_id(int(paper_id), "exact")
+    related = db.query_by_paper_id(int(paper_id), "related")
     if args.debug:
-        db_res, query_time = db_res
-
-    if not db_res:
-        return jsonify("[ERROR]: could not find paper_id ({}) in database".format(paper_id)), 400
-
-    with open(db_res[0]["pdf"], "rb") as pdf:
-        pdf_bytes = pdf.read()
-    res = make_response(pdf_bytes)
-    res.headers['Content-Type'] = 'application/pdf'
-    res.headers['Content-Disposition'] = 'inline; filename={}.pdf'.format(db_res[0]["title"])
-    return res
-
-@app.route('/article/pdf_by_title/<title>')
-def article_pdf_by_title(title):
-    '''
-    Serve PDF files from the file system based on PDF title in database.
-
-    Parameters:
-        title: PDF title in database
-
-    Example: http://localhost:5000/article/pdf_by_title/Phasor_Neural_Networks
-    '''
-    db_res = db.query_by_title(title, "exact")
-    if args.debug:
-        db_res, query_time = db_res
-
-    if not db_res:
-        return jsonify("[ERROR]: could not find title ({}) in database".format(title)), 400
-
-    with open(db_res[0]["pdf"], "rb") as pdf:
-        pdf_bytes = pdf.read()
-    res = make_response(pdf_bytes)
-    res.headers['Content-Type'] = 'application/pdf'
-    res.headers['Content-Disposition'] = 'inline; filename={}.pdf'.format(title)
-    return res
-
-@app.route('/visualization/<id>')
-def visualization(id):
-    itself = db.query_by_paper_id(int(id), "exact")
-    knn = itself+db.query_by_paper_id(int(id), "related")
+        itself, query_time = itself
+        related, query_time = related
+    knn = itself + related
 
     coords = []
     for paper in knn:
@@ -183,10 +141,31 @@ def visualization(id):
 def topic_terms(topic_id):
     return jsonify(db.topic_terms[topic_id])
 
-print(args.convert)
-db = database.get_db(model_path=args.model_path, debug_info=args.debug, build_nodes=args.nodes,
-                    num_neighbours = args.neighbours, lsi_dims = args.numLSI, lda_dims = args.numLDA,
-                    train_model=args.train, pdf_path=args.pdf_path, text_path=args.text_path,
-                    convert_pdfs=args.convert) # persistent connection to database at app start
-atexit.register(database.close_db, db) # close database connection at app exit
-app.run()
+if __name__ == "__main__":
+    if not args.pdf_path and (args.nodes or args.convert):
+        raise RuntimeError("PDF path required `-pdf <path>` for options --nodes / --convert")
+    if not args.text_path and (args.train or args.convert):
+        raise RuntimeError("Text path required `-text <path>` for options --train / --convert")
+
+    # persistent connection to database at app start
+    db = database.get_db(
+        uri=args.db_uri,
+        user=args.user,
+        password=args.password,
+        num_neighbours=args.neighbours,
+        lsi_dims=args.numLSI,
+        lda_dims=args.numLDA,
+        pdf_path=args.pdf_path,
+        text_path=args.text_path,
+        model_path=args.model_path,
+        debug_info=args.debug,
+        train_model=args.train,
+        build_nodes=args.nodes,
+        convert_pdfs=args.convert
+    )
+
+    # close database connection at app exit
+    atexit.register(database.close_db, db)
+
+    # start app
+    app.run(debug=args.debug)
--- webapp/server/database.py
+++ webapp/server/database.py
@@ -11,43 +11,42 @@ db_driver_path = Path(__file__).resolve().parents[2] / "db_and_model"
 sys.path.insert(1, str(db_driver_path))
 from db_driver import DbDriver
 
-db_uri = "bolt://localhost:7687"
-db_account = {
-    "user": "neo4j",
-    "password": "capstone"
-}
-
-def get_db(uri=db_uri, user=db_account["user"],
-           password=db_account["password"], num_neighbours=25, lsi_dims=10, lda_dims=10,
-           pdf_path="", text_path="", model_path="", debug_info=False, build_nodes = True, train_model = True,
-           convert_pdfs = True):
+def get_db(uri, user, password, num_neighbours, lsi_dims, lda_dims, pdf_path,
+           text_path, model_path, debug_info, train_model, build_nodes,
+           convert_pdfs):
     '''
-    Connect to neo4j database
+    Connect to neo4j database and builds if desired
 
     Parameters:
         uri: [string] Database URI
         user: [string] Database account username
         password: [string] Database account password
-        num_neighbours: [int] Number of nearest neighbours to compute between points in graph database
-        lsi_dims: [int] Number of dimensions for latent semantic indexing
+        num_neighbours: [int] Number of connections for documents in graph database
+        lsi_dims: [int] Number of dimensions for topic modelling (LSI)
+        lda_dims: [int] Number of dimensions for topic modelling (LDA)
         pdf_path: [string] Path to top-level directory to recursively load PDF files from
         text_path: [string] Path to directory which stores all parsed PDF text files
         model_path: [string] Path to directory which stores saved model files
         debug_info: [bool] Whether to turn on or off debug info for database
+        train_model: [bool] Whether to train new model or load existing
+        build_nodes: [bool] Whether to create new database nodes or not
+        convert_pdfs: [bool] Whether to convert PDFs to text files or load existing
     Returns:
         db: DbDriver instance
     '''
     print("[INFO]: Connecting to database")
     db = DbDriver(uri, user, password, num_neighbours, lsi_dims, lda_dims, pdf_path,
                   text_path, model_path, debug_info)
-    
+
+    # build the database with supplied arguments
+
     if build_nodes:
         db.destroy_db()
-    
-    #build the database with supplied arguments
-    print(convert_pdfs)
+
     db.build_db(train_model, build_nodes, convert_pdfs)
-    db.build_knn_graph()
+
+    if build_nodes:
+        db.build_knn_graph()
 
     return db
 
